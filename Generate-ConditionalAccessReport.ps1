663ff4b06cb2919d7250bcc9f042e72b6bcca2da
Donovan du Val <49915971+Donovand4@users.noreply.github.com>
2020-04-02 06:49:11 +0100

    Update Generate-ConditionalAccessReport.ps1
    Added getting started to the notes section of the help documentation.

--- Generate-ConditionalAccessReport.ps1
+++ Generate-ConditionalAccessReport.ps1
@@ -47,7 +47,18 @@
 .OUTPUTS
     Exports .html and .csv files that contains the Conditional Access policies.
 .NOTES
-    The script will connect to the Microsoft Graph service and collect the required information. 
+    The script will connect to the Microsoft Graph service and collect the required information.
+    Below is a list of steps recommended to allow the report to authenticate and retrieve the information required.
+	Steps:
+	1.  Download and install the Microsoft Graph PowerShell module by launching a PowerShell Console as Administrator 
+	    and execute the below command:        
+		Install-Module Microsoft.Graph        
+	2.  Launch a PowerShell Console
+	3.  Run the below to authenticate to the Microsoft Graph PowerShell (Preview) Application that is added to Azure AD.
+        	Connect-Graph -Scopes "Policy.Read.All","Directory.Read.All"        
+	4.  Complete the authentication by following the device login prompts.
+	5.  Review and accept the required permissions.
+	6.  The report can be executed as soon as the permissions are granted on the application.
 .LINK
     Github 
     https://github.com/microsoftgraph/msgraph-sdk-powershell 
