ef5423d8c6bea635e21d83c3fe0d8fdccb72db4e
Donovan du Val <49915971+Donovand4@users.noreply.github.com>
2020-05-13 15:57:25 +0100

    Updated Process and Requires Options
    The PowerShell Module has been updated and the team has modified the structure of the output data. All conditions is now located within the Conditions array. 
    Removed Requires Administrator: This can cause confusion with some modules running in the currently logged on users context and not the administrator context.

--- Generate-ConditionalAccessReport.ps1
+++ Generate-ConditionalAccessReport.ps1
@@ -22,15 +22,13 @@
 #   specified at http://www.microsoft.com/info/cpyright.htm.                # 
 #                                                                           #  
 #   Author: Donovan du Val                                                  #  
-#   Version 1.0         Date Last Modified: 31 March 2020                   #  
+#   Version 1.0         Date Last Modified: 20 March 2020                   #  
 #                                                                           #  
 #############################################################################  
 .SYNOPSIS
     PowerShell Script used to generate Conditional Access Policies.
     Created by: Donovan du Val
-    Date: 20 March 2020
-    Updated: 31 March 2020
-    Changes: Improved Error Checking on Users and Groups.
+    Date: 13 May 2020
 .DESCRIPTION
     The script will generate a report for all the Conditional Access Policies used in the Azure AD Tenant.
 .EXAMPLE
@@ -47,21 +45,12 @@
 .OUTPUTS
     Exports .html and .csv files that contains the Conditional Access policies.
 .NOTES
-    The script will connect to the Microsoft Graph service and collect the required information.
-    Below is a list of steps recommended to allow the report to authenticate and retrieve the information required.
-	Steps:
-	1.  Download and install the Microsoft Graph PowerShell module by launching a PowerShell Console as Administrator 
-	    and execute the below command:        
-		Install-Module Microsoft.Graph        
-	2.  Launch a PowerShell Console
-	3.  Run the below to authenticate to the Microsoft Graph PowerShell (Preview) Application that is added to Azure AD.
-        	Connect-Graph -Scopes "Policy.Read.All","Directory.Read.All"        
-	4.  Complete the authentication by following the device login prompts.
-	5.  Review and accept the required permissions.
-	6.  The report can be executed as soon as the permissions are granted on the application.
+    The script will connect to the Microsoft Graph service and collect the required information. 
 .LINK
     Github 
     https://github.com/microsoftgraph/msgraph-sdk-powershell 
+    Microsoft Graph PowerShell Module
+    https://www.powershellgallery.com/packages/Microsoft.Graph
 #>
 [CmdletBinding()]
 param (
@@ -71,17 +60,12 @@ param (
   )
 #Requires -Version 5.1
 #Requires -Modules Microsoft.Graph.Identity.ConditionalAccess,Microsoft.Graph.Identity.ServicePrincipal,Microsoft.Graph.Users.User,Microsoft.Graph.Groups.Group,Microsoft.Graph.Identity.RoleManagement
-#Requires -RunAsAdministrator
 Begin {    
     Clear-Host
     write-host "Importing the modules..."
-    ##Install-Module Microsoft.Graph
-    ##Import-Module Microsoft.Graph
     Import-Module Microsoft.Graph.Identity.ConditionalAccess,Microsoft.Graph.Identity.ServicePrincipal,Microsoft.Graph.Users.User,Microsoft.Graph.Groups.Group,Microsoft.Graph.Identity.RoleManagement
 
     write-host "Logging into Microsoft Graph" -ForegroundColor Green
-
-    
 
     if ((Connect-Graph -Scopes "Policy.Read.All","Directory.Read.All") -eq $null) 
     {
@@ -197,7 +181,7 @@ header {
 
 
 }
-  
+
 process {
     Write-Host ""
     Write-host "Collecting Named Locations..." -ForegroundColor Green
@@ -215,24 +199,24 @@ process {
         "State" = $pol.state
         "ID"  = $pol.id
         "createdDateTime" = if ($pol.createdDateTime){$pol.createdDateTime} else {"Null"}          
-        "ModifiedDateTime"  = if ($pol.createdDateTime){$pol.ModifiedDateTime} else {"Null"}
-        "UserIncludeUsers"  = if ($pol.UserIncludeUsers) {($pol.UserIncludeUsers | ForEach-Object{(Report-Users -ID $_ )}) -join ","} else {"Not Configured"} 
-        "UserExcludeUsers"  = if ($pol.UserExcludeUsers) {($pol.UserExcludeUsers | ForEach-Object{(Report-Users -ID $_ )}) -join ","} else {"Not Configured"} 
-        "UserIncludeGroups" = if ($pol.UserIncludeGroups) {($pol.UserIncludeGroups | ForEach-Object{(Report-Groups -ID $_ )}) -join ","} else {"Not Configured"}
-        "UserExcludeGroups" = if ($pol.UserExcludeGroups) {($pol.UserExcludeGroups | ForEach-Object{(Report-Groups -ID $_ )}) -join ","} else {"Not Configured"}
-        "UserIncludeRoles"  = if ($pol.UserIncludeRoles) {($pol.UserIncludeRoles | ForEach-Object{(Get-MgRoleManagementDirectoryRoleDefinition -UnifiedRoleDefinitionId $_).displayName}) -join "," } else {"Not Configured"}
-        "UserExcludeRoles" = if ($pol.UserExcludeRoles) {($pol.UserExcludeRoles | ForEach-Object{(Get-MgRoleManagementDirectoryRoleDefinition -UnifiedRoleDefinitionId $_).displayName}) -join "," } else {"Not Configured"}
-        "ConditionSignInRiskLevels" = if ($pol.ConditionSignInRiskLevels) {$pol.ConditionSignInRiskLevels -join ","} else {"Not Configured"}
-        "ConditionClientAppTypes" = if ($pol.ConditionClientAppTypes) {$pol.ConditionClientAppTypes -join ","} else {"Not Configured"}
-        "PlatformIncludePlatforms"  = if ($pol.PlatformIncludePlatforms) {$pol.PlatformIncludePlatforms -join ","} else {"Not Configured"}
-        "PlatformExcludePlatforms"  = if ($pol.PlatformExcludePlatforms) {$pol.PlatformExcludePlatforms -join ","} else {"Not Configured"}
-        "DeviceStateIncludeStates"  = if ($pol.DeviceStateIncludeStates) {$pol.DeviceStateIncludeStates -join ","} else {"Not Configured"}
-        "DeviceStateExcludeStates"  = if ($pol.DeviceStateExcludeStates) {$pol.DeviceStateExcludeStates -join ","} else {"Not Configured"}
-        "ApplicationIncludeApplications" = if ($pol.ApplicationIncludeApplications) {($pol.ApplicationIncludeApplications | ForEach-Object {Report-DirectoryApps -AppID $_}) -join ","} else {"Not Configured"}
-        "ApplicationExcludeApplications" = if ($pol.ApplicationExcludeApplications) {($pol.ApplicationExcludeApplications | ForEach-Object {Report-DirectoryApps -AppID $_}) -join ","} else {"Not Configured"}
-        "ApplicationIncludeUserActions" = if ($pol.ApplicationIncludeUserActions) {$pol.ApplicationIncludeUserActions -join ","} else {"Not Configured"}
-        "LocationIncludeLocations"  = if ($pol.LocationIncludeLocations) {($pol.LocationIncludeLocations | ForEach-Object {Report-NamedLocations -ID $_}) -join ","} else {"Not Configured"}
-        "LocationExcludeLocations"  = if ($pol.LocationExcludeLocations) {($pol.LocationExcludeLocations | ForEach-Object {Report-NamedLocations -ID $_}) -join ","} else {"Not Configured"}
+        "ModifiedDateTime"  = if ($pol.ModifiedDateTime){$pol.ModifiedDateTime} else {"Null"}
+        "UserIncludeUsers"  = if ($pol.Conditions.UserIncludeUsers) {($pol.Conditions.UserIncludeUsers | ForEach-Object{(Report-Users -ID $_ )}) -join ","} else {"Not Configured"} 
+        "UserExcludeUsers"  = if ($pol.Conditions.UserExcludeUsers) {($pol.Conditions.UserExcludeUsers | ForEach-Object{(Report-Users -ID $_ )}) -join ","} else {"Not Configured"} 
+        "UserIncludeGroups" = if ($pol.Conditions.UserIncludeGroups) {($pol.Conditions.UserIncludeGroups | ForEach-Object{(Report-Groups -ID $_ )}) -join ","} else {"Not Configured"}
+        "UserExcludeGroups" = if ($pol.Conditions.UserExcludeGroups) {($pol.Conditions.UserExcludeGroups | ForEach-Object{(Report-Groups -ID $_ )}) -join ","} else {"Not Configured"}
+        "UserIncludeRoles"  = if ($pol.Conditions.UserIncludeRoles) {($pol.Conditions.UserIncludeRoles | ForEach-Object{(Get-MgRoleManagementDirectoryRoleDefinition -UnifiedRoleDefinitionId $_).displayName}) -join "," } else {"Not Configured"}
+        "UserExcludeRoles" = if ($pol.Conditions.UserExcludeRoles) {($pol.Conditions.UserExcludeRoles | ForEach-Object{(Get-MgRoleManagementDirectoryRoleDefinition -UnifiedRoleDefinitionId $_).displayName}) -join "," } else {"Not Configured"}
+        "ConditionSignInRiskLevels" = if ($pol.Conditions.SignInRiskLevels) {$pol.Conditions.SignInRiskLevels -join ","} else {"Not Configured"}
+        "ConditionClientAppTypes" = if ($pol.Conditions.ClientAppTypes) {$pol.Conditions.ClientAppTypes -join ","} else {"Not Configured"}
+        "PlatformIncludePlatforms"  = if ($pol.Conditions.PlatformIncludePlatforms) {$pol.Conditions.PlatformIncludePlatforms -join ","} else {"Not Configured"}
+        "PlatformExcludePlatforms"  = if ($pol.Conditions.PlatformExcludePlatforms) {$pol.Conditions.PlatformExcludePlatforms -join ","} else {"Not Configured"}
+        "DeviceStateIncludeStates"  = if ($pol.Conditions.DeviceStateIncludeStates) {$pol.Conditions.DeviceStateIncludeStates -join ","} else {"Not Configured"}
+        "DeviceStateExcludeStates"  = if ($pol.Conditions.DeviceStateExcludeStates) {$pol.Conditions.DeviceStateExcludeStates -join ","} else {"Not Configured"}
+        "ApplicationIncludeApplications" = if ($pol.Conditions.ApplicationIncludeApplications) {($pol.Conditions.ApplicationIncludeApplications | ForEach-Object {Report-DirectoryApps -AppID $_}) -join ","} else {"Not Configured"}
+        "ApplicationExcludeApplications" = if ($pol.Conditions.ApplicationExcludeApplications) {($pol.Conditions.ApplicationExcludeApplications | ForEach-Object {Report-DirectoryApps -AppID $_}) -join ","} else {"Not Configured"}
+        "ApplicationIncludeUserActions" = if ($pol.Conditions.ApplicationIncludeUserActions) {$pol.Conditions.ApplicationIncludeUserActions -join ","} else {"Not Configured"}
+        "LocationIncludeLocations"  = if ($pol.Conditions.LocationIncludeLocations) {($pol.Conditions.LocationIncludeLocations | ForEach-Object {Report-NamedLocations -ID $_}) -join ","} else {"Not Configured"}
+        "LocationExcludeLocations"  = if ($pol.Conditions.LocationExcludeLocations) {($pol.Conditions.LocationExcludeLocations | ForEach-Object {Report-NamedLocations -ID $_}) -join ","} else {"Not Configured"}
         "GrantControlBuiltInControls" = if ($pol.GrantControlBuiltInControls) {$pol.GrantControlBuiltInControls -join ","} else {"Not Configured"}
         "GrantControlTermsOfUse"  = if ($pol.GrantControlTermsOfUse) {$pol.GrantControlTermsOfUse -join "," } else {"Not Configured"}
         "GrantControlOperator"  = if ($pol.GrantControlOperator) {$pol.GrantControlOperator} else {"Not Configured"}
